a8484a0b258721c7c803c7a4d6f99f3a9bf69a24
thuntn-dgh <thuntn.vndigitech@gmail.com>
2023-12-13 21:37:48 +0700

    impr: change info in file README

--- src/common/utils/constants.js
+++ src/common/utils/constants.js
@@ -1,6 +1,6 @@
 export const url_github = "https://github.com/charles-thuntn";
 export const url_linkedIn = "https://www.linkedin.com/in/thu-nguyen-a8954129a/";
-export const url_portfolio = "https://charlesnguyen.vercel.app";
+export const url_portfolio = "https://thuntn.vercel.app";
 export const numberphone = "0326217691";
 export const email = "nthu240@gmail.com";
 export const name_resume = "Resume-Thu-Nguyen";
